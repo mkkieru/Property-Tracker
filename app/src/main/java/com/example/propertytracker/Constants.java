bb6e9be29369bf0eb4161e1dca28ef5932be5a5e
mkkieru <mkieru55@gmail.com>
2021-09-27 21:56:00 +0300

    Add login and create account page

---
+++ app/src/main/java/com/example/propertytracker/Constants.java
@@ -0,0 +1,7 @@
+package com.example.propertytracker;
+
+public class Constants {
+    public static final String BASE_URL = "https://mark-android-api.herokuapp.com/";
+    public static final String FIREBASE_CHILD_RESTAURANTS = "properties";
+
+}
