0e9dc95d5741d08f2e6096e4ef47878ab0079c01
Steve-njeri <stevenjeri254@gmail.com>
2021-09-28 20:16:02 +0300

    created an Empty activity in our _ui package_ called PropertyDetailActivity and generated a layout file

---
+++ app/src/main/java/com/example/propertytracker/ui/PropertyDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.propertytracker.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.propertytracker.R;
+
+public class PropertyDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property_detail);
+    }
+}
