b54918dbf4739b33affd046f50f802a4e15e9558
Steve-njeri <stevenjeri254@gmail.com>
2021-09-30 15:13:07 +0300

    Modified MainActivity file

--- app/src/main/java/com/example/propertytracker/ui/MainActivity.java
+++ app/src/main/java/com/example/propertytracker/ui/MainActivity.java
@@ -22,6 +22,7 @@ import com.example.propertytracker.adapters.PropertyListAdapter;
 import com.example.propertytracker.models.Property;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -70,10 +71,27 @@ public class MainActivity extends AppCompatActivity {
         addNewProperty.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-                adminAddProperty(userId);
+                startActivity(new Intent(getApplicationContext(),AddPropertyActivity.class));
             }
         });
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String userId = user.getUid();
+        DocumentReference df = FirebaseFirestore.getInstance().collection("users").document(userId);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userLevel = documentSnapshot.getString("UserLevel");
+                if ( userLevel.equals("admin")) {
+                    addNewProperty.setVisibility(View.VISIBLE);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please Contact Admin to be able to add property",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 
     @Override
@@ -100,20 +118,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void adminAddProperty(String uid) {
-        DocumentReference df = FirebaseFirestore.getInstance().collection("users").document(uid);
-        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                String userLevel = documentSnapshot.getString("UserLevel");
-                if ( userLevel.equals("admin")) {
-                    startActivity(new Intent(getApplicationContext(),AddPropertyActivity.class));
-                } else {
-                    Toast.makeText(getApplicationContext(), "Please Contact Admin to be able to add property",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
+//    private void adminAddProperty(String uid) {
+//        DocumentReference df = FirebaseFirestore.getInstance().collection("users").document(uid);
+//        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+//            @Override
+//            public void onSuccess(DocumentSnapshot documentSnapshot) {
+//                String userLevel = documentSnapshot.getString("UserLevel");
+//                if ( userLevel.equals("admin")) {
+//                    startActivity(new Intent(getApplicationContext(),AddPropertyActivity.class));
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Please Contact Admin to be able to add property",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
 
 
     private void fetchPropertyList() {
